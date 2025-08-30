e9d700ab6d4d5c2a170d030ac445d18874b7cbdb
rabi993 <rabi.21.pust@gmail.com>
2025-08-30 18:35:38 +0600

    dragon-news-code started 52-3 & 52-4 Router Setup and folder structure analysis

---
+++ src/layouts/HomeLayout.jsx
@@ -0,0 +1,17 @@
+import Header from "../components/Header";
+
+
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <header>
+                <Header></Header>
+            </header>
+            <nav></nav>
+            <main></main>
+        </div>
+    );
+};
+
+export default HomeLayout;
