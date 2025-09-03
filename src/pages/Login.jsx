2bff97ebdb3310ca221d51dce59e8aada8caad0e
rabi993 <rabi.21.pust@gmail.com>
2025-09-03 16:30:30 +0600

    52-5-8 implement redirects on login and do form validation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,15 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../provider/AuthProvider';
 
 const Login = () => {
     const { userLogin, setUser } = useContext(AuthContext);
+    const [error, setError] = useState({});
+    const location = useLocation();
+    const navigate = useNavigate();
+    console.log(location);
+
+
     const handleSubmit = (e) => {
         e.preventDefault();
         const form = e.target;
@@ -14,11 +20,13 @@ const Login = () => {
             .then((result) => {
                 const user = result.user;
                 setUser(user);
+                navigate(location?.state ? location.state : "/")
             })
-            .catch((error)=>{
-                const errorCode= error.code;
-                const errorMessage= error.message;
-                alert(errorCode);
+            .catch((err) => {
+                // const errorCode= error.code;
+                // const errorMessage= error.message;
+                // alert(errorCode);
+                setError({ ...error, login: err.code });
             })
     }
     return (
@@ -37,6 +45,13 @@ const Login = () => {
                             <span className="label-text">Password</span>
                         </label>
                         <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                        {
+                            error.login && (
+                                <label className="label text-sm text-red-600 ">
+                                  {error.login}
+                                </label>
+                            )
+                        }
                         <label className="label">
                             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                         </label>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,17 +1,22 @@
 
-import { use, useContext } from 'react';
+import { use, useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../provider/AuthProvider';
 
 const Register = () => {
 
-    const { createNewUser,  setUser } = useContext(AuthContext);
+    const { createNewUser, setUser } = useContext(AuthContext);
+    const [error, setError] = useState({});
 
     const handleSubmit = (e) => {
         e.preventDefault();
         //get form data
         const form = new FormData(e.target);
         const name = form.get("name");
+        if (name.length < 5) {
+            setError({ ...error, name: "must be more than 5 character long"});
+            return;
+        }
         const photo = form.get("photo");
         const email = form.get("email");
         const password = form.get("password");
@@ -25,58 +30,72 @@ const Register = () => {
                 console.log(user);
                 // ...
             })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.log(errorCode, errorMessage );
+            .catch((err) => {
+                // const errorCode = error.code;
+                // const errorMessage = error.message;
+                // console.log(errorCode, errorMessage);
                 // ..
+                setError({ ...error, login: err.code });
             });
     }
-return (
-    <div className='min-h-screen flex justify-center items-center'>
-        <div className="card bg-base-100 w-full max-w-lg shrink-0 shadow-2xl p-10">
-            <h2 className='text-2xl font-semibold text-center'>Register your account</h2>
-            <form onSubmit={handleSubmit} className="card-body">
-                <div className="form-control flex flex-col">
-                    <label className="label">
-                        <span className="label-text">Name</span>
-                    </label>
-                    <input type="text" name='name' placeholder="Name" className="input input-bordered" required />
-                </div>
-                <div className="form-control flex flex-col">
-                    <label className="label">
-                        <span className="label-text">Photo URL</span>
-                    </label>
-                    <input type="text" name='photo' placeholder="Photo" className="input input-bordered" required />
-                </div>
-                <div className="form-control flex flex-col">
-                    <label className="label">
-                        <span className="label-text">Email</span>
-                    </label>
-                    <input type="email" name='email' placeholder="email" className="input input-bordered" required />
-                </div>
-                <div className="form-control  flex flex-col">
-                    <label className="label">
-                        <span className="label-text">Password</span>
-                    </label>
-                    <input type="password" name='password' placeholder="password" className="input input-bordered" required />
-                    <label className="label">
-                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                    </label>
-                </div>
-                <div className="form-control mt-6">
-                    <button className="btn btn-neutral round-none">Register</button>
-                </div>
-            </form>
-            <p className='text-center font-semibold'>
-                Already Have An Account ?{" "}
-                <Link className="text-green-500" to="/auth/login"  >
-                    Login
-                </Link>
-            </p>
+    return (
+        <div className='min-h-screen flex justify-center items-center'>
+            <div className="card bg-base-100 w-full max-w-lg shrink-0 shadow-2xl p-10">
+                <h2 className='text-2xl font-semibold text-center'>Register your account</h2>
+                <form onSubmit={handleSubmit} className="card-body">
+                    <div className="form-control flex flex-col">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" name='name' placeholder="Name" className="input input-bordered" required />
+                    </div>
+                    {
+                        error.name && (<label className='label text-xs text-rose-500'>{error.name}</label>)
+                    }
+                    <div className="form-control flex flex-col">
+                        <label className="label">
+                            <span className="label-text">Photo URL</span>
+                        </label>
+                        <input type="text" name='photo' placeholder="Photo" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control flex flex-col">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control  flex flex-col">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control  flex flex-col">
+                        {
+                            error.login && (
+                                <label className="label text-sm text-red-600 ">
+                                    {error.login}
+                                </label>
+                            )
+                        }
+                    </div>
+
+                    <div className="form-control mt-6">
+                        <button className="btn btn-neutral round-none">Register</button>
+                    </div>
+                </form>
+                <p className='text-center font-semibold'>
+                    Already Have An Account ?{" "}
+                    <Link className="text-green-500" to="/auth/login"  >
+                        Login
+                    </Link>
+                </p>
+            </div>
         </div>
-    </div>
-);
+    );
 };
 
 export default Register;
